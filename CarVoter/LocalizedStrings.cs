f85d00b8dc8cea7979f77220928db3d490de1cc7
ltran161 <lauren.tran.161@gmail.com>
2014-07-16 14:04:58 -0700

    Initial Commit

---
+++ CarVoter/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using CarVoter.Resources;
+
+namespace CarVoter
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ CarVoter/Viewmodel/OverviewViewmodel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CarVoter.Model;
+
+namespace CarVoter.Viewmodel
+{
+    public class OverviewViewmodel
+    {
+        public IList<Car> Cars { get; set; } 
+    }
+}
